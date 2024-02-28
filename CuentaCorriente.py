f9e5096dc0d41df46dff2e470baf5f3ee4c4e5b3
oskar-ortiz <okyjulian994@gmail.com>
2024-02-28 00:03:26 -0500

    Segundo

---
+++ CuentaCorriente.py
@@ -0,0 +1,22 @@
+class cuentaCorriente:
+ '''-------------------------------------------------------------
+ # Atributos
+-----------------------------------------------------------------'''
+    
+cedula=0
+nombres=0
+mes_actual=0
+
+
+'''----------------------------------------------------------------
+Metodos
+-------------------------------------------------------------------'''
+
+def visualizar_dinero(self, visualizar_dinero):
+   self.iniciar_cuenta = visualizar_dinero
+
+def depositar_dinero(self, monto_dinero):
+   self.depositar_dinero = monto_dinero
+
+def retirar.dinero(self, monto_deldinero):
+self.sacar_dinero = monto_del_dinero
